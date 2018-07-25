41f074dd4a14d45c5dd7ea3538758b98d016c68f
Thashin Naidoo <Thashin@live.com>
2018-07-25 14:18:20 +0800

    Implement basic functionality for Score and Course controllers

---
+++ TheWeekendGolfer/Controllers/CourseController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheWeekendGolfer.Data;
+
+namespace TheWeekendGolfer.Controllers
+{
+    public class CourseController : Controller
+    {
+        CourseAccessLayer _courseAccessLayer;
+
+        public CourseController(CourseAccessLayer courseAccessLayer)
+        {
+            _courseAccessLayer = courseAccessLayer;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCourses()
+        {
+            try
+            {
+                return Ok(_courseAccessLayer.GetAllCourses());
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        // GET: Course/Details/5
+        public ActionResult Details(Guid id)
+        {
+            try
+            {
+                return Ok(_courseAccessLayer.GetCourse(id));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ TheWeekendGolfer/Controllers/ScoreController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheWeekendGolfer.Data;
+using TheWeekendGolfer.Web.Data;
+using TheWeekendGolfer.Web.Models;
+
+namespace TheWeekendGolfer.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ScoreController : Controller
+    {
+        ScoreAccessLayer _scoreAccessLayer;
+
+        public ScoreController(ScoreAccessLayer scoreAccessLayer)
+        {
+            _scoreAccessLayer = scoreAccessLayer;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllScores()
+        {
+            try
+            {
+                return Ok(_scoreAccessLayer.GetAllScores());
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        // GET: Score/Details/5
+        public ActionResult Details(Guid id)
+        {
+            try
+            {
+                return Ok(_scoreAccessLayer.GetScore(id));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        // POST: Score/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([FromQuery]Score score)
+        {
+            if (_scoreAccessLayer.AddScore(score))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+
+        // POST: Score/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Score score)
+        {
+            if (_scoreAccessLayer.UpdateScore(score))
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        //[HttpDelete]
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+    }
+}
