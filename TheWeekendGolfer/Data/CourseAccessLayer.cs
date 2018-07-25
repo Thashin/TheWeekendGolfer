8fb09e1ecb258aad4b1ed4179d44380e619aad3f
Thashin Naidoo <Thashin@live.com>
2018-07-25 13:47:09 +0800

    Implement Course Access Layer

--- TheWeekendGolfer/Data/CourseAccessLayer.cs
+++ TheWeekendGolfer/Data/CourseAccessLayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TheWeekendGolfer.Web.Data;
+using TheWeekendGolfer.Web.Models;
 
 namespace TheWeekendGolfer.Data
 {
@@ -15,5 +16,70 @@ namespace TheWeekendGolfer.Data
             _context = context;
         }
 
+        public Course GetCourse(Guid id)
+        {
+            try
+            {
+                return (Course)_context.Courses.Select(s => s.Id.Equals(id));
+            }
+            catch
+            {
+                throw new Exception("Could not retrieve Course for " + id.ToString());
+            }
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            try
+            {
+                return _context.Courses.Select(s => s);
+            }
+            catch
+            {
+                throw new Exception("Could not retrieve all Courses");
+            }
+        }
+
+        public Boolean AddCourse(Course Course)
+        {
+            try
+            {
+                _context.Courses.Add(Course);
+
+                return true;
+            }
+            catch
+            {
+                throw new Exception("Could not add Course for " + Course.Id.ToString());
+            }
+        }
+
+        public Boolean UpdateCourse(Course Course)
+        {
+            try
+            {
+                _context.Courses.Update(Course);
+
+                return true;
+            }
+            catch
+            {
+                throw new Exception("Could not update Course for " + Course.Id.ToString());
+            }
+        }
+
+        public Boolean DeleteCourse(Course Course)
+        {
+            try
+            {
+                _context.Courses.Remove(Course);
+
+                return true;
+            }
+            catch
+            {
+                throw new Exception("Could not delete Course for " + Course.Id.ToString());
+            }
+        }
     }
 }
